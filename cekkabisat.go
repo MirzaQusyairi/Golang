36467a328a831a80284b11fe96ebc4a5b63304dc
MirzaQusyairi <mirza4909@gmail.com>
2020-02-26 15:08:04 +0700

    Update cekkabisat.go

--- cekkabisat.go
+++ cekkabisat.go
@@ -1,1 +1,18 @@
+/*
+Program untuk mengecek apakah tahun yang diinputkan termasuk tahun kabisat atau tidak,
+yang menghasilkan output berupa true atau false
+*/
 
+package main 
+import "fmt"
+
+func main(){
+	var tahun int
+	var proses bool
+
+	fmt.Print("Tahun: ")
+	fmt.Scanln(&tahun)
+	proses = tahun%400 == 0 || tahun%4 == 0 && tahun%100 > 0
+	fmt.Println("Kabisat: ",proses)
+	fmt.Scanln()
+}
